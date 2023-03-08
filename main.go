2d8749b4ec30b782571696570fe1fe105c1a67bf
Gilfoyle3301 <criptobes3301@gmail.com>
2023-03-08 13:55:46 +0300

    add deferedd function

--- main.go
+++ main.go
@@ -16,13 +16,13 @@ func main() {
 	for i := 1; i <= 65535; i++ {
 		wg.Add(1)
 		go func(j int) {
+			defer wg.Done()
 			var destination string = fmt.Sprintf("%s:%d", adress, j)
 			connection, errs := net.Dial("tcp", destination)
 			if errs == nil {
 				fmt.Printf("port:%d connection succssesful\n", j)
 				connection.Close()
 			}
-			wg.Done()
 		}(i)
 
 	}
