bf5d7611ee221c7c8b52aba813644cf6705f00a0
Gilfoyle3301 <criptobes3301@gmail.com>
2023-03-12 19:01:47 +0300

    Pattern worker pull

--- main.go
+++ main.go
@@ -4,28 +4,45 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"sync"
+	"runtime"
 )
+
+func workerPool(ports <-chan int, result chan<- int, adress string) {
+	for port := range ports {
+		var destination string = fmt.Sprintf("%s:%d", adress, port)
+		connection, errs := net.Dial("tcp", destination)
+		if errs != nil {
+			continue
+		}
+		result <- port
+		connection.Close()
+	}
+}
 
 func main() {
 	var (
-		adress string = os.Args[1]
-		wg     sync.WaitGroup
+		adress string   = os.Args[1]
+		ports  chan int = make(chan int, 200)
+		result chan int = make(chan int, 200)
 	)
 
-	for i := 1; i <= 65535; i++ {
-		wg.Add(1)
-		go func(j int) {
-			defer wg.Done()
-			var destination string = fmt.Sprintf("%s:%d", adress, j)
-			connection, errs := net.Dial("tcp", destination)
-			if errs == nil {
-				fmt.Printf("port:%d connection succssesful\n", j)
-				connection.Close()
-			}
-		}(i)
+	defer close(ports)
+	defer close(result)
 
+	fmt.Println("Possible numbers workers is: ", runtime.NumCPU())
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go workerPool(ports, result, adress)
 	}
-	wg.Wait()
+
+	go func() {
+		for port := 1; port <= 65535; port++ {
+			ports <- port
+		}
+	}()
+
+	for openPort := range result {
+		fmt.Printf("Port %d is open\n", openPort)
+	}
 
 }
